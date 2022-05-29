006844a72460e5eaf540418f3bd7dec3ff5b56c8
adifachanda <79835729+adifachanda@users.noreply.github.com>
2022-05-29 19:47:41 +0700

    Update Jawaban.R

--- Jawaban.R
+++ Jawaban.R
@@ -1,29 +1,25 @@
 #Nomor 1
   #a
-  Responden = c(1, 2, 3, 4, 5, 6, 7, 8, 9)
-  x = c(78, 75, 67, 77, 70, 72, 78, 74, 77)
-  y = c(100, 95, 70, 90, 90, 90, 89, 90, 100)
+  sebelum <- c(78, 75, 67, 77, 70, 72, 28, 74, 77)
+  sesudah <- c(100, 95, 70, 90, 90, 90, 89, 90, 100)
 
-  Data_Frame = data.frame(Responden, x, y)
+  data <- data.frame(
+       group = rep(c("sebelum", "sesudah"), each = 9),
+       saturation = c(sebelum, sesudah))
+  
+  print(data)
+  
+  sd(sebelum)
 
-  sdev = sd(Data_Frame$x-Data_Frame$y)
-  sdev
+  sd(sesudah)
   
   #b
-  h = Data_Frame$y - Data_Frame$x
-  t_mu = mean(h)
-  t_zbar = mean(h[1:6])
-  t_n = 6
-  t_s = sd(h[1:6])
-  t = (t_zbar - t_mu) / (t_s/sqrt(t_n))
-  t
-  pval = 2 * pt(-abs(t), df = t_n - 1)
-  pval
+  t.test(sebelum, sesudah, alternative = "greater", var.equal = FALSE)
   
   #c
-  install.packages("BSDA")
-  library(BSDA)
-  t.test(h, alternative = 'two.sided', mu = t_mu)
+  var.test(sebelum, sebelum)
+  
+  t.test(sebelum, sesudah, var.equal = TRUE)
   
 #Nomor2
   zsum.test(mean.x=23500, sigma.x = 3900, n.x = 100,  
